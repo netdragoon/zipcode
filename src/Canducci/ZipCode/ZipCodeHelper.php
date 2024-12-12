f356bba37f1ea5c0f450fc09f0dff06c4366a87f
fulviocanducci <fulviocanducci@hotmail.com>
2024-12-12 16:03:58 -0300

    packagist update version

--- src/Canducci/ZipCode/ZipCodeHelper.php
+++ src/Canducci/ZipCode/ZipCodeHelper.php
@@ -11,7 +11,11 @@ if (!function_exists('zipcode')) {
         if (function_exists('app')) {
             $zip_code = app('Canducci\ZipCode\Contracts\ZipCodeContract');
         } else {             
-            $cache = new \PhpExtended\SimpleCache\SimpleCacheFilesystem(new PhpExtended\File\FileSystem("./tests/tmp"));
+            $path = str_replace('Canducci\ZipCode', 'tmp', __DIR__);
+            if (file_exists($path) === false){
+                mkdir($path);
+            }
+            $cache = new \PhpExtended\SimpleCache\SimpleCacheFilesystem(new \PhpExtended\File\FileSystem($path));
             $request = new \Canducci\ZipCode\ZipCodeRequest();
             $zip_code = new \Canducci\ZipCode\ZipCode($cache, $request);
         }
